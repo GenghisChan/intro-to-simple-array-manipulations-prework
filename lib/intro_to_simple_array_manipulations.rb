e904dc151b371f11ce82d64a73057a53ecbb2c3c
Christofer Chan <devchumbing@gmail.com>
2018-06-15 02:59:19 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,3 +21,23 @@ end
 def shift_with_args(array)
   array.shift(2)
 end
+
+def using_concat(array, array2)
+  array.concat(array2)
+end
+
+def using_insert(array, element)
+  array[4].insert(element)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten()
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
